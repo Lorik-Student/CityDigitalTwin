20f86f7248f10ebb79bac36981037aa2d838c19a
Lorik-Student <lk73410@ubt-uni.net>
2026-05-27 13:58:30 +0200

    [Database(PostgreSQL)] Define Cities table & inital cities seed data

--- database/migrations/V1__city_table.sql
+++ database/migrations/V1__city_table.sql
@@ -1,9 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;     -- for gen_random_uuid()
+--CREATE EXTENSION IF NOT EXISTS postgis;      -- recommended for map data
+
+
 CREATE TABLE Cities (
-    id SERIAL PRIMARY KEY,
+    uuid UUID DEFAULT gen_random_uuid() UNIQUE PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     lat DOUBLE PRECISION NOT NULL,
-    long DOUBLE PRECISION NOT NULL,
+    lng DOUBLE PRECISION NOT NULL,
     weather_condition VARCHAR(40) DEFAULT 'Normal',
     population INT NOT NULL CHECK(population >= 0),
+    area INT NOT NULL CHECK(area >= 0),
     details TEXT
 )
---
+++ database/migrations/V2__add_cities.sql
@@ -0,0 +1,7 @@
+INSERT INTO Cities (name, lat, lng, weather_condition, population, area, details)
+VALUES 
+('Prizren', 42.2139, 20.7397, 'E butë', 178000, 640, 'E njohura për trashëgiminë e saj kulturore, përfshirë xhamitë, kishat dhe lagjet historike.'),
+('Prishtina', 42.6629, 21.1655, 'E butë', 200000, 572, 'The capital and largest city of Kosovo, serving as the economic and administrative hub.'),
+('Gjakova', 42.3803, 20.4308, 'E butë', 94000, 587, 'Known for its Grand Bazaar, historical monuments, and traditional craftsmanship.'),
+('Peja', 42.6593, 20.2887, 'E butë', 96000, 603, 'Located at the foot of the Rugova Canyon, known for its tourism and natural landscapes.'),
+('Mitrovica', 42.8914, 20.8660, 'E butë', 72000, 350, 'Industrial city divided by the Ibar River, historically centered around the Trepça mining complex.');
